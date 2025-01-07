0141594152c08d24ada10efcc856bb47092076a6
rishipandey2 <rishipandeyinfo@gmail.com>
2025-01-07 07:30:18 +0530

    Splash scree , Intent, Listview added

---
+++ app/src/main/java/com/example/demoapp/MainActivity.kt
@@ -0,0 +1,35 @@
+package com.example.demoapp
+
+import android.content.Intent
+import android.net.Uri
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.Button
+import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        Thread.sleep(3000)
+        installSplashScreen()
+        setContentView(R.layout.activity_main)
+
+        // Explicit Intent
+        val explicitButton = findViewById<Button>(R.id.explicitButton)
+        explicitButton.setOnClickListener {
+            val explicitInent = Intent(this, ScreenActiviy2::class.java)
+            startActivity(explicitInent)
+            finish()
+        }
+
+        // Implicit Intent
+
+        val url = "https://chatgpt.com/c/677c7c11-74bc-8012-a105-345071b35f36"
+        val implicitButton = findViewById<Button>(R.id.implicitButton)
+
+        implicitButton.setOnClickListener {
+            val implicitIntent = Intent(Intent.ACTION_VIEW, Uri.parse(url))
+            startActivity(implicitIntent)
+        }
+    }
+}
---
+++ app/src/main/java/com/example/demoapp/ScreenActiviy2.kt
@@ -0,0 +1,32 @@
+package com.example.demoapp
+
+import android.annotation.SuppressLint
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.ArrayAdapter
+import android.widget.ListView
+import android.widget.Toast
+
+class ScreenActiviy2 : AppCompatActivity() {
+    @SuppressLint("MissingInflatedId")
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_screen_activiy2)
+
+        val listView: ListView = findViewById(R.id.listView)
+        val listItems = arrayOf("Read a book" ,
+                "Create a Project" ,
+                        "Learn Kotlin" ,
+                                "Go for shopping" ,
+            "Attend a seminar")
+
+
+        val listAdapter = ArrayAdapter(this, android.R.layout.simple_list_item_1, listItems)
+        listView.adapter = listAdapter
+
+   listView.setOnItemClickListener{parent, view, position, id ->
+       val selectedItem = parent.getItemAtPosition(position) as String
+       Toast.makeText(this,"You have clicked on: $selectedItem", Toast.LENGTH_SHORT).show()
+   }
+    }
+}
